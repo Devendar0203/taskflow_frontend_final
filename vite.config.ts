185261e09a924ef266c7e85266d4802c99b3de88
[Devendar0203] <[devendarbandaru05@gmail.com]>
2026-04-15 17:25:15 +0530

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base: "/TASK-FLOW-FRONTEND/",   // ✅ ADD THIS (VERY IMPORTANT)
+  base: "/",
 
   server: {
     proxy: {
